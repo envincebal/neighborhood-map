d319b5a280b1a51d0aa183b67c2d5c0fb66265d0
Vincent Yan <vincentyan1@gmail.com>
2017-02-08 15:36:04 -0800

    Added location model data

--- js/app.js
+++ js/app.js
@@ -1,7 +1,51 @@
-// Powell's Books
-// Ground Kontrol
-// Portland Art Museum
-// Roseland Theater
-// Voodoo Doughnuts
-// Arlene Schnitzer Concert Hall
-// Pioneer Place
+// Locations Model
+var locations = [
+{
+	title: "Powell's Books",
+	address: "1005 W Burnside St, Portland, OR 97209"
+	lat: 45.523096,
+	lng: -122.681354,
+	marker: ""
+},
+{
+	title: "Ground Kontrol",
+	address: "511 NW Couch St, Portland, OR 97209"
+	lat: 45.523943,
+	lng: -122.675872,
+	marker: ""
+},
+{
+	title: "Portland Art Museum",
+	address: "1219 SW Park Ave, Portland, OR 97205"
+	lat: 45.51615,
+	lng: -122.683357,
+	marker: ""
+},
+{
+	title: "Roseland Theater",
+	address: "8 NW 6th Ave, Portland, OR 97209"
+	lat: 45.52328,
+	lng: -122.676297,
+	marker: ""
+},
+{
+	title: "Voodoo Doughnuts",
+	address: "22 SW 3rd Ave, Portland, OR 97204"
+	lat: 45.52328,
+	lng: -122.681354,
+	marker: ""
+},
+{
+	title: "Arlene Schnitzer Concert Hall",
+	address: "1037 SW Broadway, Portland, OR 97205"
+	lat: 45.517197,
+	lng: -122.681419,
+	marker: ""
+},
+{
+	title: "Pioneer Place",
+	address: "700 SW 5th Ave, Portland, OR 97204"
+	lat: 45.518335,
+	lng: -122.677261,
+	marker: ""
+}]
